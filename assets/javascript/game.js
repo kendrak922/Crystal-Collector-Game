50cc6b34971d27efe281993a6a5e461aca88d104
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-09 11:30:56 -0600

    added pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,11 @@
 'use strict';
+
+$(document).ready(function() {
+
+ let randNumber = "";
+ const crystalOne = "";
+ const crystalTwo = "";
+ const crystalThree = "";
+ const crystalFour = "";   
+
+}
